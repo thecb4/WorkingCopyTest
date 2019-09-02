bbfe4d38f4bbb62724e069ccd70c506462be0c10
The CB4 <cavelle@thecb4.io>
2019-09-02 14:11:17 -0400

    Added swift logging dependency

--- Sources/WorkingCopyTest/main.swift
+++ Sources/WorkingCopyTest/main.swift
@@ -1,1 +1,8 @@
-print("Hello, world!")
+// 1) let's import the logging API package
+import Logging
+
+// 2) we need to create a logger, the label works similarly to a DispatchQueue label
+let logger = Logger(label: "com.example.BestExampleApp.main")
+
+// 3) we're now ready to use it
+logger.info("Hello World!")
